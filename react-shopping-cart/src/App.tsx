e85578a24577899bc5625f5878f9fb642a14df22
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-01-23 22:12:58 -0500

    remove items from cart

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -56,7 +56,20 @@ const App = () => {
     });
   };
 
-  const handleRemoveFromCart = () => null;
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((acc, item) => {
+        if (item.id === id) {
+          // if it is the last item, remove from cart
+          if (item.amount === 1) return acc;
+
+          return [...acc, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...acc, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong</div>;
