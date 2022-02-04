f54e98c0220879ab9da109c49022cbc39ce450ea
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-02-03 19:23:01 -0500

    handle done and delete item

--- taskify/src/components/SingleTodo.tsx
+++ taskify/src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
 import './styles.css';
+import TodoList from './TodoList';
 
 type Props = {
   todo: Todo;
@@ -11,17 +12,35 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        // toggle isDone for matching list item
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    // keep all but the item matching the id
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className='todos__single'>
-      <span className='todos__single--text'>{todo.todo}</span>
+      {todo.isDone ? (
+        <s className='todos__single--text'>{todo.todo}</s>
+      ) : (
+        <span className='todos__single--text'>{todo.todo}</span>
+      )}
       <div>
         <span className='icon'>
           <AiFillEdit />
         </span>
         <span className='icon'>
-          <AiFillDelete />
+          <AiFillDelete onClick={() => handleDelete(todo.id)} />
         </span>
-        <span className='icon'>
+        <span className='icon' onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
