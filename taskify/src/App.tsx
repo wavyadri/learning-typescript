894c0a4e6998db729a3922b5353d3baf4fc8fa61
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-02-04 19:43:15 -0500

    implement drag and drop

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -3,7 +3,7 @@ import InputField from './components/InputField';
 import TodoList from './components/TodoList';
 import React, { useState } from 'react';
 import { Todo } from './model';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
@@ -19,8 +19,43 @@ const App: React.FC = () => {
     }
   };
 
+  const onDragEnd = (results: DropResult) => {
+    const { source, destination } = results;
+
+    // drop in null space
+    if (!destination) return;
+
+    // drop in same spot
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add,
+      active = todos,
+      complete = completedTodos;
+
+    if (source.droppableId === 'TodosList') {
+      add = active[source.index];
+      active.splice(source.index, 1); // remove
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === 'TodosList') {
+      active.splice(destination.index, 0, add); // add
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className='App'>
         <h1 className='heading'>Taskify</h1>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
