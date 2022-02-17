73d99e65609fc7ff91633df2700135c18536e6ce
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-02-17 18:42:17 -0500

    implement next question button

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -61,7 +61,16 @@ const App = () => {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // move to next question if not the last question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className='App'>
